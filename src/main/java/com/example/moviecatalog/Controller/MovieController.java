b4b6e5116435cd5629e4f6bbfa8515d751f2669c
Arthur Benjamins Minja <63090908+arthur-cmd@users.noreply.github.com>
2023-04-12 11:51:59 +0300

    rest controller changes

--- src/main/java/com/example/moviecatalog/Controller/MovieController.java
+++ src/main/java/com/example/moviecatalog/Controller/MovieController.java
@@ -3,24 +3,48 @@ package com.example.moviecatalog.Controller;
 
 
 import com.example.moviecatalog.Data.CatalogItems;
+import com.example.moviecatalog.Data.Movie;
+import com.example.moviecatalog.Data.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
 public class MovieController {
 
 
+
     @GetMapping ("{userid}")
     public List<CatalogItems> getCatalog(@PathVariable ("userid")  String userid){
-    return Collections.singletonList(
-            new CatalogItems("Transformers","Movie about robots turning into cars",8)
-    );
+        RestTemplate restTemplate= new RestTemplate();
+        //get rated movie ids
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("56y7",9),
+                new Rating("o107",6)
+
+        );
+
+
+        //get information of each rated movie ids
+        return ratings.stream().map(rating ->{
+               Movie movie = restTemplate.getForObject("http://localhost:8080/MovieInfo/"+ rating.getMovieId(),Movie.class);
+               return new CatalogItems(movie.getName(), movie.getDescription(), rating.getRating());
+                } ) .collect(Collectors.toList());
+
+
+        //put them all together
+
+
+
 
     }
 }
